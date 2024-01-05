241d9279fa5322bd0530b6deee7d2a3c5e22368a
selimdilsad <selimdilsadercan@gmail.com>
2024-01-05 14:48:36 +0300

    feat: added chat components

---
+++ components/ChatHeader.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import ChatPresence from "./ChatPresence";
+
+interface Props {
+  // user: User | undefined;
+  user: any;
+}
+
+function ChatHeader({ user }: Props) {
+  const router = useRouter();
+
+  const handleLoginWithGithub = () => {
+    //   const supabase = supabaseBrowser();
+    //   supabase.auth.signInWithOAuth({
+    //     provider: "github",
+    //     options: { redirectTo: location.origin + "/auth/callback" }
+    //   });
+  };
+
+  const handleLogout = async () => {
+    // const supabase = supabaseBrowser();
+    // await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  return (
+    <div className="h-20">
+      <div className="p-5 border-b flex items-center justify-between h-full">
+        <div>
+          <h1 className="text-xl font-bold">Daily Chat</h1>
+          <ChatPresence />
+        </div>
+        {user && <Button onClick={handleLogout}>Logout</Button>}
+        {!!user && <Button onClick={handleLoginWithGithub}>Login</Button>}
+      </div>
+    </div>
+  );
+}
+
+export default ChatHeader;
